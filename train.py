85637d378c4e71439d6d34a90de39c93c33f63ab
kodavatimahendra <kodavatimahendra95@gmail.com>
2017-02-27 22:22:35 +0530

    Test and Train Datasets

--- train.py
+++ train.py
@@ -15,10 +15,12 @@ import tensorflow as tf
 from tensorflow.contrib import rnn
 
 
-train_data_file = "Datasets/Training_Dataset.csv"
-test_data_file = "Datasets/Leaderboard_Dataset.csv"
-train_data_partial_file = "Datasets/Twist_final_data.csv"
-test_data_2_file = "Datasets/Final_Dataset.csv"
+#train_data_file = "Datasets/Training_Dataset.csv"
+#test_data_file = "Datasets/Leaderboard_Dataset.csv"
+#train_data_partial_file = "Datasets/Twist_final_data.csv"
+#test_data_2_file = "Datasets/Final_Dataset.csv"
+train_data_file = "Datasets/TrainDatasetFrom_Training_Dataset.csv"
+test_data_file = "Datasets/TestDatasetFrom_Training_Dataset.csv"
 
 #Parameters
 learning_rate = 3e-3
@@ -29,29 +31,34 @@ display_step = 100
 num_input = 32 # Data shape
 num_hidden = 128 # hidden layer num of features
 num_classes = 5 # 5 parties: Centaur, Cosmos, Ebony, Odyssey, Tokugawa
-num_examples = 20000
-num_batches = 20000
+num_examples = 48103
+num_batches = 100000
 num_batches_per_epoch = int(num_examples/batch_size)
 
-x = []
-y1 = []
-y2 = []
-with open(train_data_file,'rb') as csvfile:
-	csv_variable = csv.reader(csvfile, delimiter="," , quotechar="|")
-	next(csv_variable,None)                ##To skip the header in csv file
-	for i,row in enumerate(csv_variable):
-		x = row
-		if x[31] == '':                     #I have a slightly corrupted dataset :-(
-			pass
-		else:			
-			x[1] = char_party[x[1]]      	#decoding party_last_voted
-			x[28] = char_age[x[28]]      	#decoding age
-			x[31] = char_education[x[31]]   #decoding education
-			y1.append([int(i) for i in x[1:33]])
-			y2.append(char_target[x[35]])
 
-train_inputs = np.asarray(y1)	
-train_targets = np.asarray(y2)
+def readingDataset(input_file):
+	x = []
+	y1 = []
+	y2 = []
+	with open(input_file,'rb') as csvfile:
+		csv_variable = csv.reader(csvfile, delimiter="," , quotechar="|")
+		next(csv_variable,None)                ##To skip the header in csv file
+		for i,row in enumerate(csv_variable):
+			x = row
+			if x[31] == '':                     #I have a slightly corrupted dataset :-(
+				pass
+			else:			
+				x[1] = char_party[x[1]]      	#decoding party_last_voted
+				x[28] = char_age[x[28]]      	#decoding age
+				x[31] = char_education[x[31]]   #decoding education
+				y1.append([int(i) for i in x[1:33]])
+				y2.append(char_target[x[35]])
+	return (y1,y2)
+
+train_inputs, train_targets = readingDataset(train_data_file)
+test_inputs, test_targets = readingDataset(test_data_file)
+train_inputs = np.asarray(train_inputs)
+train_targets = np.asarray(train_targets)
 
 
 X = tf.placeholder(tf.float32,[None, num_input])
@@ -82,8 +89,9 @@ for batch in range(num_batches):
 	sess.run(train_step, feed_dict=train_data)
 
 	if batch%display_step == 0:
-		print("step {}/{}".format(batch, num_examples))
+		a = sess.run(accuracy, feed_dict=train_data)
+		print("step {}/{}.  training accuracy {}".format(batch, num_batches,a))
 
 
-a,c = sess.run([accuracy,cross_entrophy], feed_dict={X:train_inputs,Y_:train_targets})
+a,c = sess.run([accuracy,cross_entrophy], feed_dict={X:test_inputs,Y_:test_targets})
 print("accuracy : {}".format(a))
